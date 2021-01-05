cc89dd348b24c850e1505147c1bd49b3052a7f77
Kukjun Lee <20171655.info@edu.hanbat.ac.kr>
2021-01-05 16:29:35 +0900

    main project setting #<ver1.1>
    An error has occurred

--- merge/myWeb/app.py
+++ merge/myWeb/app.py
@@ -1,35 +1,103 @@
-from flask import Flask, render_template, jsonify, redirect, url_for, request
+from flask import Flask, render_template, request
+from bs4 import BeautifulSoup
+import numpy as np
+import requests
 
 app = Flask(__name__)
 
+#웹 크롤링 코드
+
+URL = 'http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=13&ncvContSeq=&contSeq=&board_id=&gubun='
+req = requests.get(URL)
+html = req.text
+soup = BeautifulSoup(html, 'html.parser')
+table_div = soup.find(id = "content")
+tables = table_div.find_all("table")
+time = table_div.find_all("div", {'class': "timetable"}, "p")
+time=time[0].text
+print(time)
+
+
+case_table = tables[0]
+rows = case_table.find_all(scope = 'row')
+numbers = case_table.find_all('td')
+
+rows2=[]
+for k in range(len(rows)):
+    s=rows[k].text
+    rows2.append(s)
+rows = np.array(rows2)
+
+nums = []
+for i in range(len(numbers)):
+    s=numbers[i].text
+    nums.append(s)
+nums = np.array(nums)
+
+#Flask 코드
 
 @app.route('/')
 def main():
     return render_template('main.html')
 
+
 @app.route('/all', methods=['POST'])
 def all():
     return render_template('all_case.html')
+
 
 @app.route('/index', methods=['POST'])
 def index():
     return render_template('index.html')
 
+
+@app.route('/vaccine', method=['POST'])
+def vaccine():
+    return render_template('vaccine.html')
+
+
 @app.route('/search', methods=['POST'])
 def search():
-    value = request.form['area']
-    if value == "대전시 동구":
-        return render_template('area/Dong-gu_Daejeon.html')
-    elif value == "대전시 서구":
-        return render_template('area/Seo-gu_Daejeon.html')
-    elif value == "대전시 유성구":
-        return render_template('area/Yuseong-gu_Daejeon.html')
-    elif value == "대전시 대덕구":
-        return render_template('area/Daedeok_gu_Daejeon.html')
-    elif value == "대전시 중구":
-        return render_template('area/Jung-gu_Daejeon.html')
+    userinput = request.form['area']
+    if userinput in rows:
+        k = 0
+        for k in range(len(rows)):
+            if rows[k] == userinput:
+                k = k * 8
+                print("총 확진자 - {}".format(nums[k + 3]))
+                print("전일대비증감 - {}".format(nums[k]))
+                print("격리중 - {}".format(nums[k + 4]))
+                print("격리해제 - {}".format(nums[k + 5]))
+                print("사망자 - {}".format(nums[k + 6]))
+                print("발생률 - {}".format(nums[k + 7]))
     else:
-        return render_template('area/not_implemented.html')
+        print("없는 지역 입력.")
+        raise NotImplementedError
+
+    html_text_front = """<!DOCTYPE html>
+    <html>
+        <head>
+            <title>코로나 확진자 정보</title>
+        </head>
+        <body>"""
+
+    html_text_back = """
+        </body>
+    </html>
+    """
+
+    with open('templates/area.html', 'w') as html_file:
+        html_file.write(html_text_front)
+        html_file.write("\n       <h2>{} - {}</h2>\n".format(time, userinput))
+        html_file.write("       <h5>총 확진자 - {}</h5>\n".format(nums[k + 3]))
+        html_file.write("       <h5>전일대비증감 - {}</h5>\n".format(nums[k]))
+        html_file.write("       <h5>격리중 - {}</h5>\n".format(nums[k + 4]))
+        html_file.write("       <h5>격리해제 - {}</h5>\n".format(nums[k + 5]))
+        html_file.write("       <h5>사망자 - {}</h5>\n".format(nums[k + 6]))
+        html_file.write("       <h5>발생률 - {}</h5>".format(nums[k + 7]))
+        html_file.write(html_text_back)
+    html_file.close()
+    return render_template('area/area.html')
 
 if __name__ == '__main__':
     app.run()
