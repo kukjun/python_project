9dd3f5e1472b4a7382a2ae9dbf5949da2144619e
Kukjun Lee <20171655.info@edu.hanbat.ac.kr>
2021-01-06 20:32:48 +0900

    merge_project<ver1.0>
    웹 크롤링, 웹서버를 구현하고 합쳐서 코드를 만들어냈음.
    프로그램 실행시, 웹 크롤링으로 코로나 관련 정보를 가져와 html파일로
    저장함.
    파일 저장 후 웹서버를 통해 만든 html파일을 보여줌

--- merge/myWeb/app.py
+++ merge/myWeb/app.py
@@ -5,7 +5,7 @@ import requests
 
 app = Flask(__name__)
 
-#웹 크롤링 코드
+# 웹 크롤링 코드
 
 URL = 'http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=13&ncvContSeq=&contSeq=&board_id=&gubun='
 req = requests.get(URL)
@@ -14,7 +14,7 @@ soup = BeautifulSoup(html, 'html.parser')
 table_div = soup.find(id = "content")
 tables = table_div.find_all("table")
 time = table_div.find_all("div", {'class': "timetable"}, "p")
-time=time[0].text
+time = time[0].text
 print(time)
 
 
@@ -22,9 +22,10 @@ case_table = tables[0]
 rows = case_table.find_all(scope = 'row')
 numbers = case_table.find_all('td')
 
-rows2=[]
+rows2 = []
+
 for k in range(len(rows)):
-    s=rows[k].text
+    s = rows[k].text
     rows2.append(s)
 rows = np.array(rows2)
 
@@ -34,16 +35,50 @@ for i in range(len(numbers)):
     nums.append(s)
 nums = np.array(nums)
 
-#Flask 코드
+area = ["합계", "서울", "부산", "대구", "인천", "광주", "대전", "울산", "세종", "경기", "강원", "충북", "충남", "전북", "전남", "경북", "경남", "제주"]
+
+for choice_area in area:
+    if choice_area in rows:
+        k = 0
+        for k in range(len(rows)):
+            if rows[k] == choice_area:
+                k = k * 8
+
+                html_text_front = """<!DOCTYPE html>
+                <html>
+                    <head>
+                        <title>코로나 확진자 정보</title>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>"""
+
+                html_text_back = """
+                    </body>
+                </html>
+                """
+
+                with open('templates/area/' + choice_area + '.html', "w", encoding="utf-8") as html_file:
+                    html_file.write(html_text_front)
+                    html_file.write("\n       <h2>{} - {}</h2>\n".format(time, choice_area))
+                    html_file.write("       <h5>총 확진자 - {}</h5>\n".format(nums[k + 3]))
+                    html_file.write("       <h5>전일대비증감 - {}</h5>\n".format(nums[k]))
+                    html_file.write("       <h5>격리중 - {}</h5>\n".format(nums[k + 4]))
+                    html_file.write("       <h5>격리해제 - {}</h5>\n".format(nums[k + 5]))
+                    html_file.write("       <h5>사망자 - {}</h5>\n".format(nums[k + 6]))
+                    html_file.write("       <h5>발생률 - {}</h5>".format(nums[k + 7]))
+                    html_file.write(html_text_back)
+                html_file.close()
+
+    else:
+        print("없는 지역 입력.")
+        raise NotImplementedError
+
+
+# Flask 코드
 
 @app.route('/')
 def main():
     return render_template('main.html')
-
-
-@app.route('/all', methods=['POST'])
-def all():
-    return render_template('all_case.html')
 
 
 @app.route('/index', methods=['POST'])
@@ -51,7 +86,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/vaccine', method=['POST'])
+@app.route('/vaccine', methods=['POST'])
 def vaccine():
     return render_template('vaccine.html')
 
@@ -59,45 +94,15 @@ def vaccine():
 @app.route('/search', methods=['POST'])
 def search():
     userinput = request.form['area']
-    if userinput in rows:
-        k = 0
-        for k in range(len(rows)):
-            if rows[k] == userinput:
-                k = k * 8
-                print("총 확진자 - {}".format(nums[k + 3]))
-                print("전일대비증감 - {}".format(nums[k]))
-                print("격리중 - {}".format(nums[k + 4]))
-                print("격리해제 - {}".format(nums[k + 5]))
-                print("사망자 - {}".format(nums[k + 6]))
-                print("발생률 - {}".format(nums[k + 7]))
+    if userinput in area:
+        return render_template('area/' + userinput + '.html')
     else:
-        print("없는 지역 입력.")
-        raise NotImplementedError
+        return render_template('area/non.html')
 
-    html_text_front = """<!DOCTYPE html>
-    <html>
-        <head>
-            <title>코로나 확진자 정보</title>
-        </head>
-        <body>"""
 
-    html_text_back = """
-        </body>
-    </html>
-    """
-
-    with open('templates/area.html', 'w') as html_file:
-        html_file.write(html_text_front)
-        html_file.write("\n       <h2>{} - {}</h2>\n".format(time, userinput))
-        html_file.write("       <h5>총 확진자 - {}</h5>\n".format(nums[k + 3]))
-        html_file.write("       <h5>전일대비증감 - {}</h5>\n".format(nums[k]))
-        html_file.write("       <h5>격리중 - {}</h5>\n".format(nums[k + 4]))
-        html_file.write("       <h5>격리해제 - {}</h5>\n".format(nums[k + 5]))
-        html_file.write("       <h5>사망자 - {}</h5>\n".format(nums[k + 6]))
-        html_file.write("       <h5>발생률 - {}</h5>".format(nums[k + 7]))
-        html_file.write(html_text_back)
-    html_file.close()
-    return render_template('area/area.html')
+@app.route('/all', methods=['POST'])
+def all():
+    return render_template('area/합계.html')
 
 if __name__ == '__main__':
     app.run()
